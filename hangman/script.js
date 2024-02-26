ac153780dbdaefba638d5be99172d54ddf9a8fb1
Critiqa <gwdreah@hotmail.com>
2024-02-26 15:24:27 +0000

    done_hangman

--- hangman/script.js
+++ hangman/script.js
@@ -1,13 +1,17 @@
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var index;
+var hang_index = 0;
+var misses;
 let words = [
     "about", "above", "begin", "black", "chair", "class", "could", "every", "first", "found",
     "great", "group", "house", "learn", "light", "night", "often", "paper", "place", "point",
-    "power", "right", "round", "school", "sound", "speak", "stand", "still", "table", "their",
+    "power", "right", "round", "spore", "sound", "speak", "stand", "still", "table", "their",
     "thing", "think", "three", "water", "where", "which", "white", "world", "would", "write", 
     "alive", "basic", "catch", "doubt", "earth", "enemy", "flame", "ghost", "laugh", "quiet" 
 ];
       for (let char of alphabet) {
         let button = document.createElement('button');
+        char = char.toLowerCase();
         button.id = char;
         button.textContent = char;
         button.classList.add('alphabet-button');
@@ -17,17 +21,36 @@ let words = [
 
         document.querySelector('.alphabet-container').appendChild(button);
       }
-//Don't touch things above. Please. For your own sake.
-//Your task is to create a hangman game.
-//At the start of the page, select a random number. Use a function to do so, and make sure it it is stored in a variable.
-//When you press a button, you have to check if the representation of the letter associated with that button is part of the random word.
-//If yes, reveal the letter in it's place. If not, change the hangman image to the next one, until the inevitable death that comes. 
 
+function getRandInt (min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
 
+var guessWord = words[getRandInt(0, words.length - 1)];
 
+ 
       // You'll need to define the checkGuess function 
       function checkGuess(guess) {
-        // Your logic to handle the guess here
-        // This would be the main part of the code
-        console.log("You guessed: " + guess); 
+       if (hang_index != 10){
+        index = 1;
+        misses = 0;
+        for (let check of guessWord) {
+          if (check == guess) {
+            document.getElementById("letter" + index).innerHTML = check;
+          }
+          else {
+            misses++;
+          }
+          index++;
+        }
+
+        if (misses == 5) {
+          hang_index++;
+          document.getElementById("hang_pic").src = "./Images/" + hang_index + ".jpg";
+          document.getElementById("used").innerHTML += guess + ", ";
+        }
+       }
+       if (hang_index == 10) {
+        document.getElementById("try").innerHTML = "You lose, he ded. The word was '" + guessWord + "'";
+       }
       }
