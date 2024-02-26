06c2d9a55764b89ae873576d609314171fa01767
Critiqa <gwdreah@hotmail.com>
2024-02-26 07:32:37 +0000

    Added tasks

--- hangman/script.js
+++ hangman/script.js
@@ -6,8 +6,6 @@ let words = [
     "thing", "think", "three", "water", "where", "which", "white", "world", "would", "write", 
     "alive", "basic", "catch", "doubt", "earth", "enemy", "flame", "ghost", "laugh", "quiet" 
 ];
-
-
       for (let char of alphabet) {
         let button = document.createElement('button');
         button.id = char;
@@ -19,9 +17,17 @@ let words = [
 
         document.querySelector('.alphabet-container').appendChild(button);
       }
+//Don't touch things above. Please. For your own sake.
+//Your task is to create a hangman game.
+//At the start of the page, select a random number. Use a function to do so, and make sure it it is stored in a variable.
+//When you press a button, you have to check if the representation of the letter associated with that button is part of the random word.
+//If yes, reveal the letter in it's place. If not, change the hangman image to the next one, until the inevitable death that comes. 
+
+
 
       // You'll need to define the checkGuess function 
       function checkGuess(guess) {
         // Your logic to handle the guess here
+        // This would be the main part of the code
         console.log("You guessed: " + guess); 
       }
