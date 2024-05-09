e600dc962446cd05ebe9b1cbffaf28b54ac72008
Critiqa <gwdreah@hotmail.com>
2024-05-09 10:21:45 +0100

    Added a zero check

--- Calculate_Monsters/main_game_js.js
+++ Calculate_Monsters/main_game_js.js
@@ -10,6 +10,9 @@ function clear_res(){
 }
 
 function addnum(number){
+    if (typed_num.length == 0 && number == 0){
+        return;
+    }
     if (typed_num.length < 29){
         typed_num = typed_num + String(number);
         document.getElementById("calc-upper-lower-p").innerHTML = typed_num;
