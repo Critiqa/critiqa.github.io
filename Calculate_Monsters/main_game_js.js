adf21545796152d5d20439fee886461f9d46d7fd
Critiqa <gwdreah@hotmail.com>
2024-05-10 15:35:48 +0100

    Added some functions

--- Calculate_Monsters/main_game_js.js
+++ Calculate_Monsters/main_game_js.js
@@ -1,4 +1,3 @@
-
 let typed_num;
 typed_num = "";
 
@@ -14,6 +13,8 @@ monst_eq_operator_list = ["+", "-", "*", "/"];
 monst_eq_chosen_op = monst_eq_operator_list[getRndInteger(0, 2)]
 
 let monst_eq_result;
+let is_minus = false;
+let calc_res;
 
 document.getElementById("calc-upper-upper-p").innerHTML = monst_eq_one + " " + monst_eq_chosen_op + " " + monst_eq_two;
 
@@ -23,12 +24,21 @@ function clear_res(){
     document.getElementById("calc-upper-lower-p").innerHTML = "0";
     typed_num = "";
     document.getElementById("calc-upper-table").style.background = "midnightblue";
+    document.getElementById("calc-upper-lower-negative-p").innerHTML = "";
+}
+
+function minus() {
+    if (is_minus){
+        is_minus = false;
+        document.getElementById("calc-upper-lower-negative-p").innerHTML = "";
+    }
+    else{
+        is_minus = true;
+        document.getElementById("calc-upper-lower-negative-p").innerHTML = "-";
+    }
 }
 
 function addnum(number){
-    if (typed_num.length == 0 && number == 0){
-        return;
-    }
     if (typed_num.length < 29){
         typed_num = typed_num + String(number);
         document.getElementById("calc-upper-lower-p").innerHTML = typed_num;
@@ -44,7 +54,11 @@ function check_result() {
             monst_eq_result = monst_eq_one - monst_eq_two;
             break
     }
-    if ((Number(typed_num) == monst_eq_result)){
+    calc_res = Number(typed_num);
+    if (is_minus){
+        calc_res = calc_res * -1
+    }
+    if ((calc_res == monst_eq_result)){
         document.getElementById("calc-upper-lower-p").innerHTML = "Correct";
         document.getElementById("calc-upper-table").style.background = "lightgreen";
     }
