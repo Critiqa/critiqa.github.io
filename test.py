4be2d40f9ef5e6edd5ecc72559854137f54f9453
Critiqa <118290078+Critiqa@users.noreply.github.com>
2024-12-18 10:49:04 +0000

    Just update

--- test.py
+++
@@ -1,8 +0,0 @@
-place = 1
-not_place = 0
-listed_items = [5, 14, 2, 5, 4, 11, 45]
-for aqui in range(len(listed_items)):
- not_place = place * listed_items[aqui]
- place = place + 2
-print(not_place)
-print(place)
