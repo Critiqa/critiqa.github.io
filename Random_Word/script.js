1c854e95d989efd06c9df6461ffd53b125850316
Critiqa <118290078+Critiqa@users.noreply.github.com>
2025-06-30 13:36:40 +0100

    WordDic

---
+++ Random_Word/script.js
@@ -0,0 +1,31 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+/*word generation function */
+const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+const vowels = ["a", "e", "i", "o", "u"];
+let wordLen;
+let wordRemain;
+let word = "";
+
+function wordGen(){
+ wordLen = getRndInteger(5, 9);
+    wordRemain = wordLen;
+    while (wordRemain >= 3){
+        word += consonants[getRndInteger(0, consonants.length - 1)];
+        word += vowels[getRndInteger(0, vowels.length - 1)];
+        word += consonants[getRndInteger(0, consonants.length - 1)];
+
+        wordRemain -= 3;
+    }
+    while (wordRemain >= 2){
+        word += vowels[getRndInteger(0, vowels.length - 1)];
+        word += consonants[getRndInteger(0, consonants.length - 1)];
+        wordRemain -= 2;
+    }
+    word += vowels[getRndInteger(0, vowels.length - 1)];
+    document.getElementById("genWord").innerHTML = word;
+}
+
+ wordGen()
